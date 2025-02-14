67ca39dc0a2a70de20b3bee2250352823acd162c
BugHunter-X <askzenai@gmail.com>
2025-02-14 07:16:05 +0100

    Add about.js file

---
+++ about.js
@@ -0,0 +1,29 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <Link href="/about">
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+
+export default function About() {
+  // This will cause a hydration mismatch error
+  const message = document.getElementById('message');
+  if (message) {
+    message.textContent = 'This is the About page';
+  }
+  return (
+    <div id="message"></div>
+  );
+}
+```
